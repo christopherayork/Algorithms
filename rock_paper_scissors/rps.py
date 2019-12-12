c27d371150739b821b61db1e71feefbc41a18285
Chris York <christopherayork1992@gmail.com>
2019-12-11 19:40:56 -0500

    recipe, stock, and cookies done. working on rpm

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,37 @@
 
 import sys
 
+
+def heaps_algorithm(a, n):
+    if n == 1: return n
+    for i in range(0, n - 1):
+        heaps_algorithm(a, n - 1)
+        if n % 2:
+            a[n - 1], a[i] = a[i], a[n - 1]
+        else:
+            a[n - 1], a[0] = a[0], a[n - 1]
+    heaps_algorithm(a, n - 1)
+
+
+from itertools import permutations
+
+
 def rock_paper_scissors(n):
-  pass 
+    if n == 0: return [[]]
+    rpm = ['rock', 'paper', 'scissors']
+    # result = heaps_algorithm(rpm, n)
+    perms = permutations(rpm, n)
+    result = []
+    for o in perms:
+        print(o)
+        result.append(list(o))
+    print(result)
+    return result
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
